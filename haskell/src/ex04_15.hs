84cc0542ec746514ecef39814352937f84e55de7
Mariano Casco <mariano@mailbox.org>
2024-05-25 21:25:27 -0400

    Ex.04-15 Clean up and rename some things

--- haskell/src/ex04_15.hs
+++ haskell/src/ex04_15.hs
@@ -7,20 +7,10 @@ type Id = Int
 
 data Interval = Interval Coord Coord deriving Show
 data End = Open | Close | Singular deriving Show
--- data Point = Point End deriving Show
 data Group = Group Coord [End] deriving Show
 data Maximum = Maximum Int Coord deriving Show
 
 -- The example from the book. All intervals' endpoints are distinct.
--- group . sort . combine =
--- [[Point 1 Open 10],
---  [Point 4 Open 15],
---  [Point 2 Open 20],
---  [Point 1 Close 40],
---  [Point 3 Open 50],
---  [Point 2 Close 60],
---  [Point 4 Close 70],
---  [Point 3 Close 90]]
 -- There are two intervals where a point would lie in three of the original
 -- intervals, [20,40] and [50,60]. Any one of the endpoints of these intervals
 -- would do as an answer.
@@ -28,14 +18,7 @@ example :: [Interval]
 example = [Interval 10 40, Interval 20 60, Interval 50 90, Interval 15 70]
 
 -- A trickier example. Interval endpoints coincide, and there is a single-point
--- interval.
--- group . sort . combine =
--- [[Point 1 Open 10],
---  [Point 2 Open 15],
---  [Point 2 Close 30,Point 3 Open 30,Point 4 Singular 30],
---  [Point 1 Close 40],
---  [Point 3 Close 50]]
--- There is one point where a point would lie in four of the original
+-- interval. There is one point where a point would lie in four of the original
 -- intervals, which is naturally p = 30. This is the answer we expect.
 example2 :: [Interval]
 example2 = [Interval 10 40, Interval 15 30, Interval 30 50, Interval 30 30]
@@ -68,28 +51,24 @@ groupPoints xs = fmap makeGroup groups
     makeGroup [] = error "unreachable"
 
 determinePoint :: [Group] -> Maximum
-determinePoint points = processGroups points 0 Nothing
+determinePoint groups = processGroups groups 0 Nothing
   where
     processGroups :: [Group] -> Int -> Maybe Maximum -> Maximum
-    processGroups (x:xs) counter Nothing =
-        let (opening, closing, singular) = processGroup x
+    processGroups ((Group coord points):xs) counter m =
+        let (opening, closing, singular) = processGroup points
             counter' = counter - closing + opening
             intersecting = counter + opening + singular
-            -- TODO update -1 with actual coord
-         in processGroups xs counter' (Just $ Maximum intersecting (-1))
-    processGroups (x@(Group coord points):xs) counter (Just (Maximum m p)) =
-        let (opening, closing, singular) = processGroup x
-            counter' = counter - closing + opening
-            intersecting = counter + opening + singular
-            (m', p') = if intersecting > m
-                then (intersecting, coord)
-                else (m, p)
-         in processGroups xs counter' (Just $ Maximum m' p')
+            m' = case m of
+                Nothing -> Maximum intersecting coord
+                Just (Maximum x c) | intersecting > x -> Maximum intersecting coord
+                                   | otherwise        -> Maximum x c
+         in processGroups xs counter' (Just m')
     processGroups [] _ (Just m) = m
     processGroups [] _ Nothing = error "unreachable"
 
-    processGroup :: Group -> (Int, Int, Int)
-    processGroup (Group _ xs) = go xs (0, 0, 0)
+    -- TODO do a fold pls
+    processGroup :: [End] -> (Int, Int, Int)
+    processGroup xs = go xs (0, 0, 0)
       where
         go :: [End] -> (Int, Int, Int) -> (Int, Int, Int)
         go (Open:xs') (o, c, s) = go xs' (o+1, c, s)
