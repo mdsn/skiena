19bcef56f801e50b27880162c3c533b50ca0c742
Mariano Casco <mariano@mailbox.org>
2024-05-26 11:21:49 -0400

    Ex.4-15 Use folds, clean and comment

--- haskell/src/ex04_15.hs
+++ haskell/src/ex04_15.hs
@@ -2,12 +2,19 @@ module Main where
 
 import Data.List (groupBy, sortBy)
 
+-- A coordinate is just an integer point, since these are 1-dimensional
+-- intervals.
 type Coord = Int
-type Id = Int
-
+-- Input intervals are represented as the left and right endpoints (inclusive).
 data Interval = Interval Coord Coord deriving Show
+-- Endpoints are categorized as opening an interval, closing an interval, or
+-- both at once (e.g. [1,1]).
 data End = Open | Close | Singular deriving Show
+-- After manipulating the input intervals and sorting their coordinates, they
+-- are collected into groups that meet at the same coordinate.
 data Group = Group Coord [End] deriving Show
+-- The running maximum is the number of intervals that meet at the coordinate
+-- of a specific group, along with the coordinate itself.
 data Maximum = Maximum Int Coord deriving Show
 
 -- The example from the book. All intervals' endpoints are distinct.
@@ -23,24 +30,26 @@ example = [Interval 10 40, Interval 20 60, Interval 50 90, Interval 15 70]
 example2 :: [Interval]
 example2 = [Interval 10 40, Interval 15 30, Interval 30 50, Interval 30 30]
 
--- O(n)
+-- O(n). Combine the list of input intervals into a single list of categorized
+-- coordinates.
 combine :: [Interval] -> [(End, Coord)]
-combine intervals = go intervals
+combine = foldr f []
   where
-    go :: [Interval] -> [(End, Coord)]
-    go [] = []
-    go ((Interval start end):xs)
-      | start /= end = (Open, start) : (Close, end) : go xs
-      | otherwise    = (Singular, start) : go xs
+    f (Interval start end) ys
+      | start /= end = (Open, start) : (Close, end) : ys
+      | otherwise    = (Singular, start) : ys
 
--- O(n log n), I think Haskell uses merge sort for lists.
+-- O(n log n), I think Haskell uses merge sort for lists. Sort the combined
+-- list of categorized coordinates by coordinate. This will put all the
+-- interval endpoints together.
 sortPoints :: [(End, Coord)] -> [(End, Coord)]
 sortPoints = sortBy $ \(_, x) (_, y) -> compare x y
 
--- No idea of the complexity of the implementation of this function in Haskell
--- but since we have sorted the points by the component by which we now group
--- them, this is theoretically doable in O(n) as all points in a group will lie
--- next to each other.
+-- (Theoretically O(n)). Group the categorized coordinates by coordinate.
+-- No idea of the complexity of the implementation of groupBy in Haskell but
+-- since we have sorted the points by the component by which we now group them,
+-- this is theoretically doable in O(n) as all points in a group will lie next
+-- to each other.
 groupPoints :: [(End, Coord)] -> [Group]
 groupPoints xs = fmap makeGroup groups
   where
@@ -50,31 +59,45 @@ groupPoints xs = fmap makeGroup groups
     makeGroup (x:xs') = Group (snd x) $ fmap fst (x:xs')
     makeGroup [] = error "unreachable"
 
+-- The central algorithm. Traverse the group of endpoints updating a counter of
+-- ongoing intervals at each coordinate where an endpoint lies. These are the
+-- only places where said counter can change, and since these are closed
+-- intervals, they are as good as any other point to determine the maximum
+-- number of intersecting intervals.
+--  This algorithm visits a group only once, and within each group, it visits
+-- its grouped endpoints once each. Therefore it is O(n) time on both the
+-- number of groups and of endpoints in the groups.
 determinePoint :: [Group] -> Maximum
-determinePoint groups = processGroups groups 0 Nothing
+determinePoint groups = findMaximum groups 0 Nothing
   where
-    processGroups :: [Group] -> Int -> Maybe Maximum -> Maximum
-    processGroups ((Group coord points):xs) counter m =
-        let (opening, closing, singular) = processGroup points
+    findMaximum :: [Group] -> Int -> Maybe Maximum -> Maximum
+    findMaximum ((Group coord points):xs) counter m =
+        let (opening, closing, singular) = count points
+            -- The updated counter is determined only by the number of
+            -- intervals that open and close at this coordinate.
             counter' = counter - closing + opening
+            -- The number of intersecting intervals at this coordinate is
+            -- whatever number of intervals were already running so far
+            -- (counter), plus any new intervals that open, plus any 1-point
+            -- intervals. Closing intervals have no effect because they were
+            -- already counted when being open, so they are already in counter.
             intersecting = counter + opening + singular
+            -- Update the running maximum+coordinate.
             m' = case m of
                 Nothing -> Maximum intersecting coord
                 Just (Maximum x c) | intersecting > x -> Maximum intersecting coord
                                    | otherwise        -> Maximum x c
-         in processGroups xs counter' (Just m')
-    processGroups [] _ (Just m) = m
-    processGroups [] _ Nothing = error "unreachable"
+         in findMaximum xs counter' (Just m')
+    findMaximum [] _ (Just m) = m
+    findMaximum [] _ Nothing = error "unreachable"
 
-    -- TODO do a fold pls
-    processGroup :: [End] -> (Int, Int, Int)
-    processGroup xs = go xs (0, 0, 0)
-      where
-        go :: [End] -> (Int, Int, Int) -> (Int, Int, Int)
-        go (Open:xs') (o, c, s) = go xs' (o+1, c, s)
-        go (Close:xs') (o, c, s) = go xs' (o, c+1, s)
-        go (Singular:xs') (o, c, s) = go xs' (o, c, s+1)
-        go [] (o, c, s) = (o, c, s)
+    -- Given the list of endpoints meeting at some coordinate, count how many
+    -- are opening and closing intervals, and how many are 1-point intervals.
+    count :: [End] -> (Int, Int, Int)
+    count = foldr f (0, 0, 0)
+      where f Open     (o, c, s) = (o+1, c, s)
+            f Close    (o, c, s) = (o, c+1, s)
+            f Singular (o, c, s) = (o, c, s+1)
 
 main :: IO ()
 main = do
